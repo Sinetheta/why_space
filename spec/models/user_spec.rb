429382ff00054bf9577da03fcfcc1de4079fb755
Kevin Attfield <k.attfield@gmail.com>
2020-11-10 09:48:48 -0800

    Add User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with valid attributes' do
+    let(:valid_attributes) { {
+      email: "fake@example.com",
+      password: "test1234"
+    } }
+
+    it 'is valid' do
+      user = described_class.new(valid_attributes)
+      expect(user.valid?).to be(true)
+    end
+  end
 end
