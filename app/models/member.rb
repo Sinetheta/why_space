5600a2e4389d261ed9ea19ad8bf484727aa9093f
Kevin Attfield <k.attfield@gmail.com>
2020-11-11 14:17:04 -0800

    Add Member.friends
    I'm gonna do the expedient thing here and add merge both kinds
    of friends as an array. It wouldn't be hard to write a single
    query to do this and return an AR collection but that would
    also complicate the preloading I'm doing on the profile pages.

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,15 @@ class Member < ApplicationRecord
   has_many :passions
   has_many :websites
 
+  has_many :from_friendships, class_name: 'Friendship', foreign_key: 'from_member_id'
+  has_many :to_friendships, class_name: 'Friendship', foreign_key: 'to_member_id'
+  has_many :from_members, through: :to_friendships
+  has_many :to_members, through: :from_friendships
+
   validates :full_name, presence: true
+
+  def friends
+    # TODO: single query
+    from_members + to_members
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -24,4 +24,22 @@ RSpec.describe Member, type: :model do
       expect(member.valid?).to be(false)
     end
   end
+
+  describe '#friends' do
+    let!(:member) { create(:member) }
+    let!(:invitee) { create(:member) }
+    let!(:inviter) { create(:member) }
+    let!(:strager) { create(:member) }
+
+    before do
+      create(:friendship, from_member: member, to_member: invitee)
+      create(:friendship, from_member: inviter, to_member: member)
+    end
+
+    subject { member.friends }
+
+    it 'returns all members who are friends only' do
+      expect(subject).to contain_exactly(invitee, inviter)
+    end
+  end
 end
