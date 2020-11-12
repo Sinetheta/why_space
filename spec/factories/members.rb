c051453c8a70748fbf2dfd20df575fdeb0bab2b6
Kevin Attfield <k.attfield@gmail.com>
2020-11-12 12:07:11 -0800

    Add passion_topics transient to member factory
    A few traits and transients can go a long way to unclutter spec
    setup. This will allow us to specify Passion.topic as strings
    on a member and sort out the Website/Passion required.

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,6 +1,18 @@
 FactoryBot.define do
   factory :member do
     user
+    websites { [] }
     full_name { "Louis Pasteur" }
+
+    transient do
+      passion_topics { [] }
+    end
+
+    after(:create) do |member, evaluator|
+      website = member.websites.first || create(:website, member: member)
+      evaluator.passion_topics.each do |topic|
+        member.passions.create(website: website, topic: topic)
+      end
+    end
   end
 end
